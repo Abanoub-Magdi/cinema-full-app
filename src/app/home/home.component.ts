a8ded01b9076e9de67d72e852b316295dc49c606
abanoub magdi <abanoubmagdi.4040@gmail.com>
2024-10-21 16:58:36 +0300

    update view

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,12 +10,14 @@ export class HomeComponent implements OnInit {
   isRatingFinal: boolean[] = []; // Array to track finalization for each movie
   trendingMovies: any;
   theatreMovies: any;
+  popularMovies:any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getTrendingMovies();
     this.getTheatreMovies();
+    this.getPopulareMovies();
   }
 
   getTrendingMovies() {
@@ -30,10 +32,20 @@ export class HomeComponent implements OnInit {
   getTheatreMovies() {
     this.http.get('assets/data/theatre-movies.json').subscribe((movies: any) => {
       this.theatreMovies = movies;
-      this.isRatingFinal = new Array(this.trendingMovies.length).fill(false); // Initialize with false for each movie
-      console.log(this.trendingMovies);
+      this.isRatingFinal = new Array(this.theatreMovies.length).fill(false); // Initialize with false for each movie
+      console.log(this.theatreMovies);
     });
   }
+
+
+  getPopulareMovies() {
+    this.http.get('assets/data/popular-movies.json').subscribe((movies: any) => {
+      this.popularMovies = movies;
+      this.isRatingFinal = new Array(this.popularMovies.length).fill(false); // Initialize with false for each movie
+      console.log(this.popularMovies);
+    });
+  }
+
 
 
   finalizeRating(index: number) {
